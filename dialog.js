c697fc7b0d04627770b448be76c3625156732b2c
Felix Schmoll <eggi.innovations@gmail.com>
2015-10-18 00:32:43 +0200

    Added question into dialog

--- dialog.js
+++ dialog.js
@@ -1,27 +1,59 @@
 debugger;
 
-var regExp = new RegExp("engadget");
+function createDialog() {
+  var body = $("body");
 
-if(regExp.test(document.URL)) { //url matches
-	//alert("url does match!");
-
-	var body = $("body");
-
-	var div = $("<div>")
+  //create div to block website content
+  var div = $("<div>")
     .addClass("focus")
     .css({"width": "90%",
           "height": "90%",
-          "background": 'rgb(244,0,0)',
+          "background": 'rgb(255,255,255)',
           "left": "5%",
           "top": "5%", 
+          "border": "5px solid black",
           "position": "fixed",
           "z-index": 9999999999
     })
     .appendTo("body");
 
-} else {
-	// alert("url doesn't match!");
+  var question = getRandomQuestion(6115);
+  var questionForm = $("<form role='form' id='questionForm'>");
+  questionForm.appendTo(div);
+    questionForm.css({
+      "all": "initial",
+      "font-size": "40px"
+   });
+
+  var questionGroup = $("<div>")
+    .addClass("form-group")
+    .appendTo(questionForm);
+
+  var questionLabel = $("<label for='answer' id='question'>");
+
+  questionLabel.html(question.question);
+  questionLabel.appendTo(questionGroup);
+
+
+
+  questionForm.appendTo(div);
 }
+
+function checkWebsite(){
+  var regExp = new RegExp("engadget");
+
+  if(regExp.test(document.URL)) { //url matches
+
+  	createDialog();
+
+  }
+};
+
+//if( $("#toggle").checked) {  //put in here actual code => fetch from store
+  checkWebsite();
+//} else {
+  console.log("button disabled");
+//}
 
 /**
  * @param {string} categoryId Category id to get random question
@@ -38,7 +70,7 @@ function getRandomQuestion(categoryId) {
   request.send(null);
   if(request.status == 200) {
      results = JSON.parse(request.responseText).clues;
-  }
+  } //what happens if it doesn't work?
   var rand = Math.floor((Math.random() * results.length));
   return results[rand];
 }
